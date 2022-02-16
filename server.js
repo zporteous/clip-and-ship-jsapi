dab06b469611d422ff24d108da42f1fab6b4c9d3
zach <porteous.zj@gmail.com>
2022-02-16 21:16:08 +0000

    g

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
 const axios = require('axios');
+const helmet = require("helmet");
+
+
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: false,
+//     crossOriginResourcePolicy:"cross-origin",
+//   })
+// );
 
 
 require('dotenv').config()
 
 // express stuff
 app.use(cors());
-app.use(express.static('./public'))
+app.use(express.static((__dirname+'/public')));
 app.use(express.json());
 
-
-// app.get('/clip-calcite', async (req,res) => {
-  
-//   res.send("index.html")
-// })
-
+app.get("/clip-calcite", function(req,res) {
+  console.log("accesssed")
+})
 
 app.listen(3000, function () {
   console.log('listening on port 3000')
