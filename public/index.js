06f89659c2b8204ee7b64922edd740676968fd23
zach <porteous.zj@gmail.com>
2022-02-16 09:45:50 +0000

    auth working

--- public/index.js
+++ public/index.js
@@ -2,8 +2,79 @@ require([
   "esri/widgets/Sketch",
   "esri/Map",
   "esri/layers/GraphicsLayer",
-  "esri/views/MapView"
-], (Sketch, Map, GraphicsLayer, MapView) => {
+  "esri/views/MapView",
+  "esri/identity/OAuthInfo", 
+  "esri/identity/IdentityManager",
+  "esri/portal/PortalQueryParams",
+  "esri/portal/Portal"
+], (Sketch, Map, GraphicsLayer, MapView, OAuthInfo, esriId,PortalQueryParams, Portal) => {
+
+  const info = new OAuthInfo({
+    // Swap this ID out with registered application ID
+    appId: "oskpHjPU478RAynR",
+    // Uncomment the next line and update if using your own portal
+    // portalUrl: "https://<host>:<port>/arcgis"
+    // Uncomment the next line to prevent the user's signed in state from being shared with other apps on the same domain with the same authNamespace value.
+    // authNamespace: "portal_oauth_inline",
+    popup: false
+  });
+  
+  esriId.registerOAuthInfos([info]);
+
+  esriId
+    .checkSignInStatus(info.portalUrl + "/sharing")
+    .then(() => {
+      displayItems();
+    })
+    .catch(() => {
+      // Anonymous view
+      console.log("an error occured while logging in..")
+    });
+    const signIn = document.getElementById("sign-in")
+    signIn.addEventListener("click", () => {
+      // user will be redirected to OAuth Sign In page
+      esriId.getCredential(info.portalUrl + "/sharing");
+      
+    });
+
+    const signOut = document.getElementById("sign-out")
+    signOut.addEventListener("click", () => {
+      // user will be redirected to OAuth Sign In page
+      roleDiv.style.display="none"
+      esriId.destroyCredentials();
+      window.location.reload();
+    });
+
+    function displayItems() {
+      const portal = new Portal();
+      // Setting authMode to immediate signs the user in once loaded
+      portal.authMode = "immediate";
+      // Once loaded, user is signed in
+      portal.load().then(() => {
+        // Create query parameters for the portal search
+        const queryParams = new PortalQueryParams({
+          query: "owner:" + portal.user.username,
+          sortField: "numViews",
+          sortOrder: "desc",
+          num: 20
+        });
+        const roleDiv = document.getElementById("role")
+        const userIndicator = document.getElementById("user-indicator")
+        userIndicator.username =  portal.user.username;
+        signIn.style.display="none"
+        signOut.style.display="block"
+        roleDiv.style.display="block"
+        roleDiv.innerHTML=portal.user.role
+
+
+        // anonPanelElement.style.display = "none";
+        // personalPanelElement.style.display = "block";
+
+        // // Query the items based on the queryParams created from portal above
+        portal.queryItems(queryParams);
+      });
+    }
+
   const graphicsLayer = new GraphicsLayer();
 
   const map = new Map({
