6447d4080a3785a51c0d54d5721b6913e7388ad0
zach <porteous.zj@gmail.com>
2022-02-16 21:28:03 +0000

    changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require([
 
   const info = new OAuthInfo({
     // Swap this ID out with registered application ID
-    appId: "oskpHjPU478RAynR",
+    appId: "IX43OT0wSqtqNZ2L",
     // Uncomment the next line and update if using your own portal
     // portalUrl: "https://<host>:<port>/arcgis"
     // Uncomment the next line to prevent the user's signed in state from being shared with other apps on the same domain with the same authNamespace value.
