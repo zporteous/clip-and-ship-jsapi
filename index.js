afd87a4d794c11239325c09fb9ea3ee343b50a35
Zachery Porteous <porteous.zj@gmail.com>
2022-02-20 16:47:36 -0800

    updated UI

--- index.js
+++ index.js
@@ -6,25 +6,29 @@ require([
   "esri/identity/OAuthInfo", 
   "esri/identity/IdentityManager",
   "esri/portal/PortalQueryParams",
-  "esri/portal/Portal"
-], (Sketch, Map, GraphicsLayer, MapView, OAuthInfo, esriId,PortalQueryParams, Portal) => {
+  "esri/portal/Portal",
+  "esri/request",
+  "esri/Graphic",
+  "esri/layers/support/Field",
+  "esri/layers/FeatureLayer",
+], (Sketch, Map, GraphicsLayer, MapView, 
+  OAuthInfo, esriId,PortalQueryParams, Portal, 
+  request, Graphic, Field, FeatureLayer) => {
 
+  // BEGIN AUTHENTICATION
   const info = new OAuthInfo({
-    // Swap this ID out with registered application ID
     appId: "IX43OT0wSqtqNZ2L",
-    // Uncomment the next line and update if using your own portal
     // portalUrl: "https://<host>:<port>/arcgis"
-    // Uncomment the next line to prevent the user's signed in state from being shared with other apps on the same domain with the same authNamespace value.
-    // authNamespace: "portal_oauth_inline",
     popup: false
   });
+
   const roleDiv = document.getElementById("role")
   esriId.registerOAuthInfos([info]);
 
   esriId
     .checkSignInStatus(info.portalUrl + "/sharing")
     .then(() => {
-      displayItems();
+      getInfo();
     })
     .catch(() => {
       // Anonymous view
@@ -45,36 +49,23 @@ require([
       window.location.reload();
     });
 
-    function displayItems() {
+    function getInfo() {
       const portal = new Portal();
       // Setting authMode to immediate signs the user in once loaded
       portal.authMode = "immediate";
       // Once loaded, user is signed in
       portal.load().then(() => {
-        // Create query parameters for the portal search
-        const queryParams = new PortalQueryParams({
-          query: "owner:" + portal.user.username,
-          sortField: "numViews",
-          sortOrder: "desc",
-          num: 20
-        });
-        
         const userIndicator = document.getElementById("user-indicator")
         userIndicator.username =  portal.user.username;
         signIn.style.display="none"
         signOut.style.display="block"
         roleDiv.style.display="block"
         roleDiv.innerHTML=portal.user.role
-
-
-        // anonPanelElement.style.display = "none";
-        // personalPanelElement.style.display = "block";
-
-        // // Query the items based on the queryParams created from portal above
-        portal.queryItems(queryParams);
       });
     }
 
+  // END AUTHENTICATION
+  // BEGIN MAP 
   const graphicsLayer = new GraphicsLayer();
 
   const map = new Map({
@@ -85,16 +76,17 @@ require([
   const view = new MapView({
     container: "map-view",
     map: map,
-    zoom: 5,
-    center: [90, 45]
+    zoom: 4,
+    center: [ -98.30437001245608,39.33320882729786]
   });
-
+  
   view.when(() => {
+    // make sketch widget, add to map
     const sketch = new Sketch({
       layer: graphicsLayer,
       view: view,
       layout:"vertical",
-      availableCreateTools:['rectangle'],
+      availableCreateTools:['rectangle', 'polygon'],
       // graphic will be selected as soon as it is created
       creationMode: "single",
       visibleElements:{
@@ -104,21 +96,151 @@ require([
         },
         settingsMenu: false,
         undoRedoMenu:false,
-      }
+      },
+      visible:true,
     });
+    view.ui.add(sketch, "top-left");
 
-    view.ui.add(sketch, "top-right");
+    // action button for draw + upload
+    var drawOrUpload = document.getElementById("draw-or-upload")
+    var uploadShpBtn = document.getElementById("upload-shp")
+    var uploadLoading = document.getElementById("upload-shp-status")
+    var uploadUi = document.getElementById("upload-ui")
+    uploadUi.style.opacity=.4;
+    uploadShpBtn.disabled=true;
 
+    drawOrUpload.addEventListener('click', switchOptions)
+    function switchOptions(){
+      map.layers.removeAll();
+      if(drawOrUpload.selectedItem.value === "upload") {
+        map.layers.add(graphicsLayer)
+        sketch.layer.graphics=[];
+        sketch.visible=true;
+        uploadUi.style.opacity=.4;
+      } else if (drawOrUpload.selectedItem.value === "draw") {
+        sketch.visible=false;
+        uploadShpBtn.disabled=false;
+        uploadUi.style.opacity=1;
+      }
+    }
+
+    uploadShpBtn.addEventListener('change',(event) => {
+      console.log("file event firing...")
+      const fileName = event.target.value.toLowerCase();
+      sketch.layer.graphics=[];
+      if (fileName.indexOf(".zip") !== -1) {
+        //is file a zip - if not notify user
+        uploadShpBtn.loading = true;
+        generateFeature(fileName);
+        console.log("generate feature called...")
+      } else {
+        document.getElementById("upload-shp-status").innerHTML =
+          '<p style="color:red">Add shapefile as .zip file</p>';
+      }
+    })
+
+    // erase any existing features that have been drawn
+    // show sketch UI, when complete, hide sketch UI
+    sketch.on("create", function(event) {
+      if (event.state === "complete") {
+        sketch.visible=false;
+      }
+    })
+
+    function uploadShpActions(){
+      sketch.layer.graphics=[];
+    }
+
+    function generateFeature(fileName) {
+      let name = fileName.split(".");
+      // Chrome adds c:\fakepath to the value - we need to remove it
+      name = name[0].replace("c:\\fakepath\\", "");
+      const params = {
+        name: name,
+        targetSR: view.spatialReference,
+        maxRecordCount: 1000,
+        enforceInputFileSizeLimit: true,
+        enforceOutputJsonSizeLimit: true
+      };
+
+      const myContent = {
+        filetype: "shapefile",
+        publishParameters: JSON.stringify(params),
+        f: "json"
+      };
+
+      // use the REST generate operation to generate a feature collection from the zipped shapefile
+      let portalUrl ="https://www.zjp.maps.arcgis.com"
+      request(portalUrl + "/sharing/rest/content/features/generate", {
+        query: myContent,
+        body: document.getElementById("upload-shp"),
+        responseType: "json"
+      })
+        .then((response) => {
+          console.log(response)
+          const layerName = response.data.featureCollection.layers[0].layerDefinition.name;
+          addShapefileToMap(response.data.featureCollection);
+        })
+        .catch(errorHandler);
+    };
+
+    function errorHandler(error) {
+      console.log(error)
+      uploadShpBtn.color="error";
+    }
+    var sourceGraphics = [];
+    function addShapefileToMap(featureCollection) {
+      const layers = featureCollection.layers.map((layer) => {
+        const graphics = layer.featureSet.features.map((feature) => {
+          return Graphic.fromJSON(feature);
+        });
+        sourceGraphics = sourceGraphics.concat(graphics);
+        const featureLayer = new FeatureLayer({
+          objectIdField: "FID",
+          source: graphics,
+          fields: layer.layerDefinition.fields.map((field) => {
+            return Field.fromJSON(field);
+          })
+        });
+        return featureLayer;
+        // associate the feature with the popup on click to enable highlight and zoom to
+      });
+      map.addMany(layers);
+      uploadLoading.active = false;
+      view.goTo(sourceGraphics).catch((error) => {
+        if (error.name != "AbortError") {
+          console.error(error);
+        }
+      });
+    };
   });
+  // END MAP
 
+  // CHOICES of CONTOURS AND RASTERS
+  var exportFormatContours = document.getElementById("export-format")
+  var exportFormatRasters = document.getElementById("export-format-raster")
+  var contourInterval = document.getElementById("contour-input")
+  var datasetChoice  = document.getElementById("major-radio-buttons")
+  datasetChoice.addEventListener("calciteRadioButtonGroupChange",(value)=>{
+    if (value.detail==="Rasters") {
+      contourInterval.disabled=true
+      exportFormatRasters.disabled=false
+      exportFormatContours.disabled=true
+    } else if (value.detail==="Contours") {
+      exportFormatRasters.disabled=true
+      exportFormatContours.disabled=false
+      contourInterval.disabled=false
+    }
+  })
+  var submitButtonContours = document.getElementById('submit-contours')
+
+
+  // CHANGING MODALS, SUBMISSION OF JOBS
   var submitModalContours  = document.getElementById("submit-modal-contours")
   var submitButtonContours = document.getElementById('submit-contours')
-  var submitModalImagery = document.getElementById("submit-modal-imagery")
-  var submitButtonImagery = document.getElementById('submit-imagery')
 
   submitButtonContours.addEventListener('click', changeToLoadingContours)
-  submitButtonImagery.addEventListener('click', changeToLoadingImagery)
-
+  
   function changeToLoadingContours() {
     submitButtonContours.loading = !submitButtonContours.loading;
     console.log(document.getElementById("export-format").selectedItem.value);
@@ -129,21 +251,7 @@ require([
       submitButtonContours.loading = !submitButtonContours.loading;
       submitModalContours.active = !submitModalContours.active
     }, 1000)
-    console.log("helo")
-    return
-  }
-
-  function changeToLoadingImagery() {
-    submitButtonImagery.loading = !submitButtonImagery.loading;
-    // console.log(document.getElementById("export-format").selectedItem.value);
-    document.getElementById("success-modal-imagery").active = 
-    setTimeout(()=>{
-      console.log("loaded")
-
-      submitButtonImagery.loading = !submitButtonImagery.loading;
-      submitModalImagery.active = !submitModalImagery.active
-    }, 1000)
-    console.log("helo")
+    console.log("hello")
     return
   }
 
